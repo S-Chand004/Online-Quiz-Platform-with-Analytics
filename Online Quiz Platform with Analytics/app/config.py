78111ca0df05d89da790692c099c61fb1d2dcd17
Snehashish Chand <162430375+S-Chand004@users.noreply.github.com>
2025-12-20 14:05:27 +0530

    Update SECRET_KEY in config.py

--- Online Quiz Platform with Analytics/app/config.py
+++ Online Quiz Platform with Analytics/app/config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = '344###112'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 's@chand'
+
     MYSQL_DB = 'quizdb'
