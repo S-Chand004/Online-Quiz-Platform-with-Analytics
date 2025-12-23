2ad15fd366b9163a5af74e0fdbadea0ff5f40cea
Snehashish Chand <162430375+S-Chand004@users.noreply.github.com>
2025-12-23 19:09:22 +0530

    Update config.py

--- Online Quiz Platform with Analytics/app/config.py
+++ Online Quiz Platform with Analytics/app/config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = '344###112'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 's@chand'
+    MYSQL_PASSWORD = 'password'
 
     MYSQL_DB = 'quizdb'
+
